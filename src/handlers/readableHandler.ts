1f8bdaba76b288a7d1a9072546378a118095fb73
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-24 23:27:21 +0300

    add Handler

---
+++ src/handlers/readableHandler.ts
@@ -0,0 +1,24 @@
+import { Duplex } from "stream";
+import { controller } from "../controller";
+
+export const readableHandler = (duplex: Duplex) => {
+    let data = "";
+    return async () => {
+        try {
+            let chunk;
+            while (null !== (chunk = duplex.read())) {
+                data += chunk;
+            }
+            const [command, ...pixels] = data.split(" ");
+            const args = pixels.map((num) => Number(num));
+            const result = await controller(command, args);
+            duplex.write(result);
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            }
+        } finally {
+            data = "";
+        }
+    };
+};
