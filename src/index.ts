2b20db5dc3d8a3ebb836d71b5aace748b058b6c6
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-24 23:27:02 +0300

    refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,36 @@
-import Jimp from "jimp";
 import { httpServer } from "./http_server/index";
-import robot from "robotjs";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, createWebSocketStream, AddressInfo } from "ws";
 import "dotenv/config";
-import { COMMANDS } from "./enums/commandsEnum";
-import { drawCircle } from "./helpers/drawCircle";
-import { drawSquare } from "./helpers/drawSquare";
-import { drawRectangle } from "./helpers/drawRectangle";
+import { readableHandler } from "./handlers/readableHandler";
+import { IWebSocket } from "./interfaces/IWebSocket";
+import { interval } from "./utils/interval";
+import { heartbeat } from "./utils/heartbeat";
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
-const wss = new WebSocketServer({ port: +process.env.PORT || 8080 });
+export const wss = new WebSocketServer({ port: +process.env.PORT || 8080 });
 
-wss.on("connection", (ws) => {
-    console.log("Connection enabled");
+wss.on("connection", async function (ws: IWebSocket, req) {
+    const ip = req.socket.remoteAddress;
+    const { port } = wss.address() as AddressInfo;
+    console.log(`Connection enabled! IP:${ip} PORT:${port}`);
+    ws.isAlive = true;
 
-    ws.on("message", (message) => {
-        const [command, ...pixels] = message.toString().split(" ");
-        const { x, y } = robot.getMousePos();
-        switch (command) {
-            case COMMANDS.MOUSE_POSITION: {
-                ws.send(`${command} ${x},${y}`);
-                break;
-            }
-            case COMMANDS.MOUSE_UP: {
-                robot.dragMouse(x, y - +pixels[0]);
-                ws.send(command);
-                break;
-            }
-            case COMMANDS.MOUSE_RIGHT: {
-                robot.dragMouse(x + +pixels[0], y);
-                ws.send(command);
-                break;
-            }
-            case COMMANDS.MOUSE_DOWN: {
-                robot.dragMouse(x, y + +pixels[0]);
-                ws.send(command);
-                break;
-            }
-            case COMMANDS.MOUSE_LEFT: {
-                robot.dragMouse(x - +pixels[0], y);
-                ws.send(command);
-                break;
-            }
-            case COMMANDS.DRAW_CIRCLE: {
-                drawCircle(+pixels[0]);
-                ws.send(command);
-                break;
-            }
-            case COMMANDS.DRAW_SQUARE: {
-                drawSquare(+pixels[0]);
-                ws.send(command);
-                break;
-            }
-            case COMMANDS.DRAW_RECTANGLE: {
-                console.log(message.toString());
-
-                drawRectangle(+pixels[0], +pixels[1]);
-                ws.send(command);
-                break;
-            }
-        }
+    const duplex = createWebSocketStream(ws, {
+        encoding: "utf-8",
+        decodeStrings: false,
     });
+    duplex.on("readable", readableHandler(duplex));
+    ws.on("pong", heartbeat);
+    ws.on("close", () => {
+        duplex.destroy();
+    });
+});
+wss.on("close", () => {
+    clearInterval(interval);
+});
+process.on("SIGINT", () => {
+    wss.close();
 });
